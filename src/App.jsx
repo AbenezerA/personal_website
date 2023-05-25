86ff2b412ff4820f5b1a9ee72b4e7caea18818cc
AbenezerA <ata2152@columbia.edu>
2023-05-26 00:13:14 +0300

    landing page with bio cont and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,104 @@
-import { useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import './App.css'
 import personalPhoto from './assets/personal-photo.jpg';
+import codingPhoto from './assets/coding-photo.jpg';
+
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  // const [dMode, setDMode] = useState(useLocation().state);
+
+  const dMode = useLocation().state;
+  // const location = useLocation();
+  // setDMode(location.state);
+  // console.log("dMode: ", dMode);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  // const scrollToTop = () => {
+  //   // console.log("dmode in app: ", dMode);
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth',
+  //   });
+  // };
+
+  const scrollToBio = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <div className='App'>
-        <div className="main-facade-cont">
-          <div className="mfcs-left main-facade-cont-sub">
-            <div className=""> 
-              <h4 className="font-scode me-3">Hi! My name is </h4>
+        <span className={`${dMode ? "darkmode" : "lightmode"} intro-span`}>
+          <div className="intro-span-sub">
+            <div className="intro-name-cont">
+              <h4 className="font-scode">Hi! My name is </h4>
               <h1>Abenezer Amanuel.</h1>
+              <h4 className="font-scode">I'm an aspiring Software Engineer and Fullstack Developer studying Computer Science at Columbia University in New York City.</h4>
             </div>
-            <h4 className="font-scode"> I'm an aspiring Software Engineer studying Computer Science at Columbia University in the city of New York.</h4>
+            <div className="intro-photo-cont">
+              <img className="intro-photo" src={personalPhoto} alt="Professional headshot photo of Abenezer"></img>
+            </div>
           </div>
-          <div className="main-facade-cont-sub">
-            <img className="personal-photo" src={personalPhoto} alt="professional photo of abenezer"></img>
+          <div className="scroll-cont">
+            <div className={`scroll ${dMode ? "darkmode" : "lightmode"}`} onClick={scrollToBio}></div>
           </div>
-        </div>
-        <div className="scroll-cont">
-          <div class="scroll"></div>
-        </div>
+        </span>
+        <span id="bio-span" style={{backgroundImage: `url(${codingPhoto})`, opacity: scrollPosition / 500, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+          <div className="bio-cont">
+            <div className="bio-text-cont">
+              <h6 className="font-scode">I was born and raised in Addis Ababa, Ethiopia. I later moved to New York City to pursue my BA in Computer Science at Columbia University.</h6>
+            </div>
+            <div className="bio-text-cont">
+              <h6 className="font-scode">Having just completed my second year of college, I am eager to apply my knowledge from class into solving real problems in real working conditions.</h6>
+            </div>
+            <div className="bio-text-cont">
+              <h6 className="font-scode">I am particularly interested in <u>Software Engineering</u>, <u>Fullstack Web Development</u>, and <u>Computer Vision</u>.</h6>
+            </div>
+            <div className="bio-text-cont">
+              <h6 className="font-scode">My short-term goal is to gain foundational skills in software engineering. In the long-term, I plan to work to increase education access to disadvantaged communities by building software solutions.</h6>
+            </div>
+          </div>
+          {/* <div className='scroll-up-cont'>
+            {isVisible && (
+              <button className="scroll-up-button" onClick={scrollToTop}>
+                <h6 className="font-scode">Back to top.</h6>
+              </button>
+            )}
+          </div> */}
+        </span>
     </div>
   )
 }
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,23 +1,79 @@
-import { Outlet, Link } from "react-router-dom";
-import { React } from 'react';
+import { Outlet, Link, useNavigate  } from "react-router-dom";
+import React, { useRef, useState, useEffect } from 'react';
+import { Link as ScrollLink, animateScroll } from 'react-scroll';
+
+import darkImg from '../assets/dark.png';
+import lightImg from '../assets/light.png';
 
 const Layout = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const [dMode, setDMode] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const isScrollingDown = currentScrollPos > prevScrollPos;
+
+            setVisible(isScrollingDown ? false : true);
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollPos]);
+
+    const scrollToSpecificElement = () => {
+        animateScroll.scrollTo(1000);
+    };
+
+    const setMode = () => {
+        if (dMode === false) {
+            setDMode(true);
+        } else {
+            setDMode(false);
+        }
+        console.log("dmode after: ", dMode);
+        navigate("/", {state : dMode});
+    }
 
     return (
-        <div class="layout">
-            <nav className="top-nav navbar fixed-top navbar-expand-lg text-reset">
+        <div className="layout">
+            <nav className={`${visible ? 'visible' : 'hidden'} top-nav navbar fixed-top navbar-expand-lg text-reset`}>
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler text-bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="navbar-sub collapse navbar-collapse" id="navbarSupportedContent">
-                        <Link to="/"> <h3 className="font-pstart text-black mb-0 w-200">Abenezer</h3> </Link>
+                    <div className="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+                        <Link to="/"> <h3 className="font-scode nav-item text-white mb-0 w-200">Abenezer</h3> </Link>
                         <ul className="navbar-nav justify-content-around">
                             <li className="nav-item m-3">
-                                <Link to="/"> <h5 className="font-scode text-black mb-0">About me</h5> </Link>
+                                <Link 
+                                    to="/#bio-span"
+                                    smooth={true}
+                                    duration={100}
+                                    onClick={scrollToSpecificElement}> 
+                                    <h5 className="font-scode text-white mb-0">About me</h5> 
+                                </Link>
                             </li>
                             <li className="nav-item m-3">
-                                <Link to="/new" > <h5 className=" font-scode text-black mb-0">Portfolio</h5> </Link>
+                                <Link to="/projects" > <h5 className=" font-scode text-white mb-0">Projects</h5> </Link>
+                            </li>
+                            <li className="nav-item m-3">
+                                <Link to="/contact" > <h5 className=" font-scode text-white mb-0">Contact</h5> </Link>
+                            </li>
+                            <li className="nav-item m-3">
+                                <Link to="/" > <h5 className=" font-scode text-white mb-0">Resume</h5> </Link>
+                            </li>
+                            <li className="nav-item m-3">
+                                <Link to="https://www.linkedin.com/in/abenezer-amanuel/" > <h5 className=" font-scode text-white mb-0">LinkedIn</h5> </Link>
+                            </li>
+                            <li className="nav-item m-3 dmode-btn-cont">
+                                <img src={(dMode==false) ? darkImg : lightImg} onClick={setMode}></img>
                             </li>
                         </ul>
                     </div>
